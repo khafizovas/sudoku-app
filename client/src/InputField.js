428ecbc21672c5ae5061f4a621e57c71ccea7231
khafizovas <justhomosapiens121@gmail.com>
2021-07-08 18:54:19 +0300

    refactor for loop in InputField render()

--- client/src/InputField.js
+++ client/src/InputField.js
@@ -3,16 +3,22 @@ import './InputField.css';
 
 class InputField extends React.Component {
 	render() {
+		const inputFieldNums = [];
+
+		for (let i = 0; i < 9; ++i) {
+			inputFieldNums.push(
+				<button
+					className='input-cell'
+					onClick={() => this.props.handleClick(i + 1)}
+					key={i}>
+					{i + 1}
+				</button>
+			);
+		}
+
 		return (
 			<div id='input-field'>
-				{[...Array(9).keys()].map((i) => (
-					<button
-						className='input-cell'
-						onClick={() => this.props.handleClick(i + 1)}
-						key={i}>
-						{i + 1}
-					</button>
-				))}
+				{inputFieldNums}
 				<button className='input-cell' onClick={this.props.hint}>
 					Hint
 				</button>
