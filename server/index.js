2531f3de222440cdcef089362a15f0e1d0531925
khafizovas <justhomosapiens121@gmail.com>
2021-07-22 23:55:29 +0300

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
@@ -10,4 +12,10 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
